80516bab063ed901028b8f974b663fae6cc3a723
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2017-07-22 10:25:31 +1000

    add GET by id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,11 +1,22 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    _id: new ObjectID(), 
+    text: 'First test'
+}, {
+    _id: new ObjectID(), 
+    text: 'Second test'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +35,7 @@ describe('POST /todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -44,10 +55,40 @@ describe('POST /todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(0);
                 done();
             }).catch((e) => done(e));
         });
+    });
+})
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        }).end(done);
+    });
+})
+
+describe('GET /todos/:id', () => {
+    it('should get todos doc', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            console.log(res.body.todo);
+            expect(res.body.todo.text).toBe(todos[0].text);
+        }).end(done);
+    });
+
+    it('should get 404 if pass invalud id', (done) => {
+        request(app)
+        .get('/todos/59704560e916eb3267477777')
+        .expect(404)
+        .end(done);
     });
 })
